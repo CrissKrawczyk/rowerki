d614bcfb7c951b938aa4d4aceafe2a21fcc3c038
kolarczyk <syzrkk@gmail.com>
2024-06-11 18:32:52 +0200

    Dodanie obsługi SSL

--- src/main/java/pl/rowerki/security/SecurityConfiguration.java
+++ src/main/java/pl/rowerki/security/SecurityConfiguration.java
@@ -19,19 +19,21 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
+                .requiresChannel(channel ->
+                        channel.anyRequest().requiresSecure())
                 .authorizeHttpRequests((requests) -> requests
                         .dispatcherTypeMatchers(DispatcherType.FORWARD, DispatcherType.ERROR).permitAll()
                         .requestMatchers("/", "/index.html", "/static/**").permitAll()
                         .requestMatchers("/service/admin/**").hasAuthority("admin")
                         .requestMatchers("/service/employee/**").hasAuthority("employee")
                         .requestMatchers("/service/**").authenticated()
-                        .requestMatchers(HttpMethod.GET,"/api/locations/**").hasAuthority("employee")
+                        .requestMatchers(HttpMethod.GET, "/api/locations/**").hasAuthority("employee")
                         .requestMatchers("/api/locations/**").hasAuthority("admin")
                         .requestMatchers("/api/orders/**").hasAuthority("employee")
                         .requestMatchers("/api/users/isAdmin").hasAuthority("employee")
                         .requestMatchers("/api/users/**").hasAuthority("admin")
                         .requestMatchers("/api/vehicles/**").hasAuthority("admin")
-                        .requestMatchers(HttpMethod.GET,"/api/vehicleKinds/**").hasAuthority("employee")
+                        .requestMatchers(HttpMethod.GET, "/api/vehicleKinds/**").hasAuthority("employee")
                         .requestMatchers("/api/vehicleKinds/**").hasAuthority("admin")
                         .requestMatchers("/api/workDay/**").hasAuthority("employee"))
                 .formLogin((form) -> form
